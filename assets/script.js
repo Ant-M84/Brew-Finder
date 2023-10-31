e68b6cc96c859b320e301f874c9aa337148f1786
Jbassard <jonathanacciarito@gmail.com>
2023-10-31 18:33:33 -0400

    refined initial search feature using latitude and longitude instead

--- assets/script.js
+++ assets/script.js
@@ -20,18 +20,22 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(data);
         var userCurrentCity = data.city;
         var userCurrentState = data.region;
-        console.log(
+          var userLatitude = data.latitude;
+          var userLongitude = data.longitude;
+          console.log(
           "Hello User! You're currently in " +
             userCurrentCity +
             ", " +
             userCurrentState +
             "!"
-        );
+          );
+          console.log(userLatitude);
+          console.log(userLongitude);
         //   Replacing potential spaces in city names with underscores (to use api)
         currentCityUnderscored = userCurrentCity.replace(/ /g, "_");
         // Forcing that to be lowercase -JA
         currentCityClean = currentCityUnderscored.toLowerCase();
-        fetchNearbyBars(currentCityClean);
+        fetchNearbyBars(userLatitude, userLongitude);
       })
       .catch((error) => {
         // Error handling done here -JA
@@ -39,12 +43,12 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 
-  function fetchNearbyBars(currentCityClean) {
-    const barsByCityApi =
-      // NOTE: some places only have 4 or 5 bars, but this will fetch up to 8
-      `https://api.openbrewerydb.org/v1/breweries?by_city=${currentCityClean}&per_page=8`;
+  function fetchNearbyBars(userLatitude, userLongitude) {
+    const barsByLatLonApi =
+      // NOTE: searching by city gives weird results, this automatically sorts results by NEAREST
+      `https://api.openbrewerydb.org/v1/breweries?by_dist=${userLatitude},${userLongitude}&per_page=8`;
 
-    fetch(barsByCityApi)
+    fetch(barsByLatLonApi)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -53,10 +57,33 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .then((data) => {
         // do stuff with the data here
-        console.log(data);
+          barData = data;
+          console.log(barData);
+          displayNearBars(barData);
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   }
+    
+    function displayNearBars(barData) {
+        for (i = 0; i < barData.length; i++) {
+            // Getting what we need from the data
+            barName = barData[i].name;
+            barAddress = barData[i].address_1;
+            barWebsite = barData[i].website_url;
+            barPhone = barData[i].phone;
+            // Grabbing where it gets displayed
+            nameDisplay = document.getElementById(`establishment-${[i]}`);
+            addressDisplay = document.getElementById(`address-${[i]}`);
+            websiteDisplay = document.getElementById(`website-${[i]}`);
+            phoneDisplay = document.getElementById(`phone-${[i]}`);            
+            // And displaying it!
+            nameDisplay.textContent = barName;
+            addressDisplay.textContent = barAddress;
+            websiteDisplay.textContent = barWebsite;
+            phoneDisplay.textContent = barPhone;
+            
+        }
+    }
 });
